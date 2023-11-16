df7aaf999e7e874ea6dac9571dd665d7eab40803
Diana Cuellar <dcuellar@laprensa.com.sv>
2023-11-16 11:56:36 -0600

    Adding calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,11 +1,62 @@
-import { Navbar } from "../"
+import { Calendar} from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import { addHours } from 'date-fns';
+import { Navbar } from "../"
+import { localizer, getMessagesES } from '../../helpers';
+
+
+const events = [{
+title: 'Cumpleanos',
+notes: 'Acuerdate',
+start: new Date(),
+end: addHours (new Date (),2),
+bgColor: '#fafafa',
+user: {
+  _id: '123',
+  name: 'Diana'
+}
+
+}]
 
 export const CalendarPage = () => {
+  
+  const eventStyleGetter = (event, start, end, isSelected) => {
+
+  const style = {
+    backgroundColor: '#347CF7',
+    borderRadius: '0px',
+    opacity: 0.8,
+    color: 'white'
+
+  }
+  
+    return {
+        style
+  }
+
+
+  }
+  
+  
+  
   return (
     <>
     <Navbar/>
     
+    <Calendar
+    culture='es'
+      localizer={ localizer }
+      events={ events }
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc( 100vh - 80px)'}}
+      messages={ getMessagesES() }
+      eventPropGetter={eventStyleGetter}
+
+    />
+
+
     </>
   )
 }
